5275440c2f8a5dc29fddf941eb081916362a6bc0
Voltura5 <rigvedmanoj1998@gmail.com>
2018-04-10 08:38:16 +0530

    UI improved

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+	socket.on("join", function(name){
+		console.log(name);
+	});
+  	socket.on('chat message', function(msg){
     io.emit('chat message', msg);
  });
 });
